6561119364bd179528f2bc86189f9bb75a9f6039
gdhillon2 <gman.dhillon@gmail.com>
2025-09-05 18:10:04 -0700

    basic table for storing data

---
+++ postgres/init.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA IF NOT EXISTS raw_data;
+
+CREATE TABLE IF NOT EXISTS raw_data.sensor_data (
+    id SERIAL PRIMARY KEY,
+    machine_id VARCHAR(50),
+    sensor VARCHAR(50),
+    reading DOUBLE PRECISION,
+    timestamp TIMESTAMP
+);
